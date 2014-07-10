60d4bfa53e4662290260e375855f08c40640aee6
Joo Aun Saw <jasaw@dius.com.au>
2014-07-10 12:03:12 +1000

    flatten jobs list before checking if we can map the list

--- lights/job2ledstrip.py
+++ lights/job2ledstrip.py
@@ -17,10 +17,10 @@ class Job2LedStrip(job2light_translator.Job2LightTranslator):
         self.logger = logger.Logger('JenkinsPoller')
         self.strand = strand
 
-        if len(jobs) == 0 or len(jobs) > self.strand.num_leds:
-            raise job2light_translator.InputError('Unable to map ' + len(jobs) + ' jobs to ' + self.strand.num_leds + ' LEDs')
         # order of the jobs is important
         self.jobs = list(list_utils.flatten_list(jobs))
+        if len(self.jobs) == 0 or len(self.jobs) > self.strand.num_leds:
+            raise job2light_translator.InputError('Unable to map ' + str(len(self.jobs)) + ' jobs to ' + str(self.strand.num_leds) + ' LEDs')
         self.offset = dict.fromkeys(self.jobs)
         self.leds_per_job = int(self.strand.num_leds / len(self.jobs))
 
