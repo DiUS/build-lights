b46731faf4102b34df4ecaf50ccdc00c53249518
Tarcio Saraiva <tarcio@gmail.com>
2016-10-26 17:10:33 +1100

    refactor(server): renaming server files

--- web-controller/src/server/index.js
+++ web-controller/src/server/index.js
@@ -1,54 +1,7 @@
 'use strict'
 
-require('nodejs-dashboard')
+const server = require('./server')
 
-const express = require('express')
-const powerOff = require('power-off')
-const compression = require('compression')
-const reboot = require('nodejs-system-reboot')
-const expressHandlebars = require('express-handlebars')
-
-const app = express()
-
-const handlebarsConfig = {
-  defaultLayout: 'main',
-  layoutsDir: 'src/server/views/layouts',
-  partialsDir: 'src/server/views/partials'
-}
-
-app.engine('handlebars', expressHandlebars(handlebarsConfig))
-app.set('view engine', 'handlebars')
-app.set('views', `${process.cwd()}/src/server/views`)
-
-app.use('/static', express.static(`${process.cwd()}/src/server/public`))
-app.use(compression())
-
-app.get('/', (req, res) => {
-  res.render('home')
+server.listen(3000, () => {
+  console.log('Build light web server successfully started.')
 })
-
-app.get('/reboot', (req, res) => {
-  reboot((err, stdErr, stdOut) => {
-    if (err) {
-      // TODO log error
-      return res.render('home', { error: 'Could not reboot.' })
-    }
-    res.end()
-  })
-})
-
-app.get('/shutdown', (req, res) => {
-  powerOff((err, stdErr, stdOut) => {
-    if (err) {
-      // TODO log error
-      return res.render('home', { error: 'Could not shutdown.' })
-    }
-    res.end()
-  })
-})
-
-module.exports = app
-
-// app.listen(3000, () => {
-//   console.log('Build light web server successfully started.')
-// })
