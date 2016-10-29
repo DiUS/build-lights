020d2528eefd135816d58d32055f562164489b37
Tarcio Saraiva <tarcio@gmail.com>
2016-10-30 09:13:12 +1100

    feat(logging): add request and error logging

--- web-controller/src/server/server.js
+++ web-controller/src/server/server.js
@@ -2,10 +2,12 @@
 
 require('nodejs-dashboard')
 
+const winston = require('winston')
 const express = require('express')
 const powerOff = require('power-off')
 const compression = require('compression')
 const reboot = require('nodejs-system-reboot')
+const expressWinston = require('express-winston')
 const expressHandlebars = require('express-handlebars')
 
 const app = express()
@@ -22,6 +24,23 @@ app.set('views', `${process.cwd()}/src/server/views`)
 
 app.use('/static', express.static(`${process.cwd()}/src/server/public`))
 app.use(compression())
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: false,
+      colorize: true
+    }),
+    new winston.transports.File({
+      json: true,
+      colorize: false,
+      filename: 'access.log'
+    })
+  ],
+  meta: true,
+  expressFormat: true,
+  colorize: true
+}))
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -46,5 +65,23 @@ app.get('/shutdown', (req, res) => {
     res.end()
   })
 })
+
+app.put('/save', (req, res) => {
+
+})
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: false,
+      colorize: true
+    }),
+    new winston.transports.File({
+      json: true,
+      colorize: false,
+      filename: 'error.log'
+    })
+  ]
+}))
 
 module.exports = app
