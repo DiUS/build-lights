3e5f236d76c772d18d1d62634eb90851e32d4b62
Carwin Zimmermann <carwinz@gmail.com>
2016-12-06 15:05:11 +1100

    ensure job names aren't duplicated

--- light-controller/config/json_config.py
+++ light-controller/config/json_config.py
@@ -9,16 +9,15 @@ try:
     import json
 except ImportError:
     import simplejson as json
+import jsonschema
+from jsonschema import validate
+from jsonschema import ValidationError
 
 from lib import logger
 from lib import error
 from lib import list_utils
 from lib import json_custom_decode
 
-import json
-import jsonschema
-from jsonschema import validate
-from jsonschema import ValidationError
 
 class Error(error.Generic):
     """Base class for light controller module exceptions"""
@@ -33,18 +32,23 @@ class JsonConfig(object):
     def __init__(self, config_file='config.json'):
         self.logger = logger.Logger('JsonConfig')
         self.config_file = config_file
+        self._load_config()
+        self._check_config_is_valid()
 
+    def _load_config(self):
         f = open(self.config_file, 'r')
         self.logger.log('Reading config file: %s', self.config_file)
         self.config = json.load(f, object_hook=json_custom_decode.decode_unicode_to_str_dict)
         f.close()
 
-        self._check_against_schema()
+    def _check_config_is_valid(self):
+
+        self._check_config_against_schema()
 
         if not list_utils.list_items_unique(self.config['jobs']):
             raise ConfigError('jobs must be unique.')
 
-    def _check_against_schema(self):
+    def _check_config_against_schema(self):
         with open('./config/config_schema.json') as data_file:
             schema = json.load(data_file)
         try:
--- light-controller/config/test/json_config_test.py
+++ light-controller/config/test/json_config_test.py
@@ -5,7 +5,14 @@ class JsonConfigTest(unittest.TestCase):
 
     def test_errors_if_config_violates_schema(self):
         try:
-            JsonConfig('./config/test/invalid_config.json')
+            JsonConfig('./config/test/invalid_config_missing_jobs.json')
+            self.assertTrue(False)
+        except ConfigError as ce:
+            self.assertEquals("Error in config file ./config/test/invalid_config_missing_jobs.json: u'jobs' is a required property", str(ce))
+
+    def test_errors_if_config_has_duplicated_job_names(self):
+        try:
+            JsonConfig('./config/test/invalid_config_duplicated_job_names.json')
             self.assertTrue(False)
         except ConfigError as ce:
             self.assertEquals('', ce.message)
