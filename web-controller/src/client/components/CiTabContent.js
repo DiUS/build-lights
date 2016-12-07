0ec5b13daed6d58d04db8f32761e7e4c0abd7a7e
Carwin Zimmermann <carwinz@gmail.com>
2016-12-07 14:08:55 +1100

    when the form loads, show the fields appropriate to the selected ci

--- web-controller/src/client/components/CiTabContent.js
+++ web-controller/src/client/components/CiTabContent.js
@@ -8,15 +8,30 @@ import { save } from '../sam/actions'
 import { transformFormIntoPayload } from './utils'
 
 export const CiTabContent = (model, lastUpdated) => {
+  const ciToolFields = {
+    ciAddress: ['jenkins'],
+    ciUsername: ['travisci', 'circleci'],
+    ciApiToken: ['circleci']
+  }
+
   const handleChange = (event) => {
     const ciTool = event.currentTarget.value
-    const ciUsername = document.getElementById('ciUsername')
-    ciUsername.parentNode.style.display = (['travisci', 'circleci'].indexOf(ciTool) > -1) ? null : 'none'
-    const apiToken = document.getElementById('apiToken')
-    apiToken.parentNode.style.display = (ciTool === 'circleci') ? null : 'none'
-    const ciAddress = document.getElementById('ciAddress')
-    ciAddress.parentNode.style.display = (ciTool === 'jenkins') ? null : 'none'
+    for (var key in ciToolFields) {
+      const makeVisible = ciToolFields[key].indexOf(ciTool) > -1
+      const fieldElement = document.getElementById(key)
+      if (makeVisible) {
+        fieldElement.parentNode.classList.remove('hidden')
+        fieldElement.parentNode.classList.add('shown')
+      } else {
+        fieldElement.parentNode.classList.remove('shown')
+        fieldElement.parentNode.classList.add('hidden')
+      }
+    }
   }
+
+  const ciAddressVisibility = (ciToolFields['ciAddress'].indexOf(model.tool) > -1 ? 'shown' : 'hidden')
+  const ciApiTokenVisibility = (ciToolFields['ciApiToken'].indexOf(model.tool) > -1 ? 'shown' : 'hidden')
+  const ciUsernameVisibility = (ciToolFields['ciUsername'].indexOf(model.tool) > -1 ? 'shown' : 'hidden')
 
   const handleFormSubmit = (event) => {
     let postData = { save: 'ci', payload: {} }
@@ -33,17 +48,17 @@ export const CiTabContent = (model, lastUpdated) => {
           <option value='circleci'>Circle CI</option>
           <option value='travisci'>Travis CI</option>
         </select>
-        <div class='fieldset'>
+        <div className={`fieldset ${ciAddressVisibility}`}>
           <label for='ciAddress'>Address of the <span>CI server you want to connect to</span></label>
           <input required type='text' id='ciAddress' placeholder='http://myci.mycompany' name='ciAddress' value={model.address} />
         </div>
-        <div class='fieldset'>
-          <label for='apiToken'>API token for CI account</label>
-          <input required type='text' id='apiToken' placeholder='' name='apiToken' value={model.apiToken} />
-        </div>
-        <div class='fieldset'>
+        <div className={`fieldset ${ciUsernameVisibility}`}>
           <label for='ciUsername'>Username associated with your CI</label>
           <input required type='text' id='ciUsername' name='ciUsername' value={model.username} />
+        </div>
+        <div className={`fieldset ${ciApiTokenVisibility}`}>
+          <label for='ciApiToken'>API token for CI account</label>
+          <input required type='text' id='ciApiToken' placeholder='' name='ciApiToken' value={model.apiToken} />
         </div>
       </div>
       <div className='actions'>
