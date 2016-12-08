34875c0f7d22b2adcedfe7e5c03a860116935f87
Voon Siong Wong <vwong@dius.com.au>
2016-12-08 16:39:25 +1100

    Add buildkite support

---
+++ light-controller/ci_server/buildkite.py
@@ -0,0 +1,43 @@
+import urllib, json
+from lib.constants import STATUS
+
+_STATUS = {
+    'running'   : STATUS.BUILDING_FROM_PREVIOUS_STATE,
+    'scheduled' : STATUS.UNKNOWN,
+    'passed'    : STATUS.SUCCESS,
+    'failed'    : STATUS.FAILURE,
+    'blocked'   : STATUS.UNKNOWN,
+    'canceled'  : STATUS.ABORTED,
+    'canceling' : STATUS.ABORTED,
+    'skipped'   : STATUS.ABORTED,
+    'not_run'   : STATUS.NOT_BUILT,
+    'finished'  : STATUS.UNKNOWN
+}
+
+class Source():
+
+    def __init__(self, api_token, username, endpoint=None):
+        self.api_token = api_token
+        self.username  = username
+        self.endpoint  = endpoint if endpoint else 'https://api.buildkite.com'
+
+    def list_projects(self):
+        params = {'access_token': self.api_token}
+        url = self.endpoint + "/v2/organizations/" + self.username + "/pipelines"
+        data = self._query(url, params)
+        return list(map(lambda x: x['slug'], data))
+
+    def project_status(self, pipeline):
+        params = {'access_token': self.api_token, 'branch': 'master'}
+        url = self.endpoint + "/v2/organizations/" + self.username + "/pipelines/" + pipeline + "/builds"
+        try:
+            data = self._query(url, params)
+        except Exception, e:
+            return STATUS.POLL_ERROR
+
+        state = data[0]['state']
+        return _STATUS[state]
+
+    def _query(self, url, params):
+        response = urllib.urlopen(url + "?%s" % urllib.urlencode(params))
+        return json.loads(response.read())
---
+++ light-controller/ci_server/test/buildkite_test.py
@@ -0,0 +1,19 @@
+import unittest
+from ci_server.buildkite import Source
+from lib.constants import STATUS
+
+class BuildkiteTest(unittest.TestCase):
+
+    def setUp(self):
+        api_token = 'some_token'
+        username  = 'my_username'
+        endpoint  = 'http://localhost:8000/buildkite'
+        self.source = Source(api_token, username, endpoint)
+
+    def test_list_projects(self):
+        projects = self.source.list_projects()
+        self.assertTrue('project1' in projects)
+        self.assertTrue('project2' in projects)
+
+    # def test_successful_build(self):
+    #     self.assertEqual(self.source.project_status('project1'), STATUS.SUCCESS)
