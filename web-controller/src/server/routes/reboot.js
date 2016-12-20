97045407a143ae69b0e826d9326787a8d6ca2adc
Tarcio Saraiva <tarcio@gmail.com>
2016-12-21 09:21:31 +1100

    chore(log): add logging before performing shutdown actions

--- web-controller/src/server/routes/reboot.js
+++ web-controller/src/server/routes/reboot.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const cp = require('child_process')
+const logger = require('../logger')
 
 module.exports = (router) => {
   router.get('/reboot', (req, res) => {
     cp.execSync('shutdown -k -r now')
+    logger.info('User able to perform reboot action. Rebootting the device.')
     res.json({ reboot: true, countdown: 30 })
     cp.execSync('shutdown -r now')
   })
--- web-controller/src/server/routes/shutdown.js
+++ web-controller/src/server/routes/shutdown.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const cp = require('child_process')
+const logger = require('../logger')
 
 module.exports = (router) => {
   router.get('/shutdown', (req, res) => {
     cp.execSync('shutdown -k -h now')
+    logger.info('User able to perform shutdown action. Shutting down the device.')
     res.json({ shutdown: true, countdown: 15 })
     cp.execSync('shutdown -h now')
   })
